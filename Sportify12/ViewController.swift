2e932cbfc71eb951ab86fa724948f04242a9c3fd
daniel jermaine <danieljermaine2@gmail.com>
2023-02-20 09:14:45 +0100

    Initial Commit

---
+++ Sportify12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sportify12
+//
+//  Created by Apple on 20/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
